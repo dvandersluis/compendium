7619d93308e6d0b5c204153edfbcf3f674f21b8d
Daniel Vandersluis <dvandersluis@selfmgmt.com>
2013-10-18 21:49:43 -0400

    Add ResultSet#empty?

--- lib/compendium/result_set.rb
+++ lib/compendium/result_set.rb
@@ -2,7 +2,7 @@ require 'active_support/core_ext/module/delegation'
 
 module Compendium
   class ResultSet
-    delegate :first, :last, :to_a, :each, :map, :[], :count, :length, :size, :==, to: :records
+    delegate :first, :last, :to_a, :empty?, :each, :map, :[], :count, :length, :size, :==, to: :records
 
     attr_reader :records
     alias :all :records
